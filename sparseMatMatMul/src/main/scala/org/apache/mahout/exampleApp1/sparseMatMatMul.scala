fd1e16f8402d85a072079d092c82af1866053f4b
Andrew Palumbo <apalumbo@apache.org>
2019-09-20 06:45:23 -0700

    mend
    inital commit

--- sparseMatMatMul/src/main/scala/org/apache/mahout/exampleApp1/sparseMatMatMul.scala
+++ sparseMatMatMul/src/main/scala/org/apache/mahout/exampleApp1/sparseMatMatMul.scala
@@ -14,18 +14,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
-package org.aoache.mahout
 
-import org.apache.mahout.math.
+package org.apache.mahout.exampleApp1
+
+import org.apache.log4j.Logger
+import org.apache.mahout._
+import common.RandomUtils
+import org.apache.mahout.math._
+import {Matrices, Matrix}
+import drm._
+import scalabindings._
+import RLikeOps._
+import DrmLike._
 
 
-class exampleApp1(val _m: Int,
+
+class sparseMatMatMul(val _m: Int,
                   val _n: Int,
                   val _s: Int,
-                  val _para: Int,
-                  var _pctDense: Double = .01,
-                  var _seed: Long = 1234L) {
+                  val _para: Int = 5,
+                  val _pctDense: Double = .01,
+                  val _seed: Long = 1234L) {
 
+val log: Logger = Logger.getLogger(this.getClass())
 
 def timeSparseDRMMMul(m: Int = _m,
                       n: Int = _n,
@@ -66,8 +77,17 @@ def timeSparseDRMMMul(m: Int = _m,
 
 }
 
-  object exampleApp1 {
+  object sparseMatMatMul {
     def main(args: Array[String]): Unit = {
-      new exampleApp1.timeSparseDRMMMul(1000,2000,23000,)
+    val sMmm = new sparseMatMatMul(300000,
+                          300000,
+                          300000,
+                          5,
+                          .01,
+                          1234L)
+      val wallClopckTime: Long = sMmm.timeSparseDRMMMul()
+      val log = Logger.getLogger(sMmm.getClass)
+      log.trace(String.format("tWallclock Tome to multiply drmA %*% drmB =  $L",wallClopckTime))
+
     }
 }
