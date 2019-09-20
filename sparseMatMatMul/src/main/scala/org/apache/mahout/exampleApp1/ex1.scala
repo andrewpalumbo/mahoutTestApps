cb1f9b4bb0a4cd90610a101b9fb6448c42223825
Andrew Palumbo <apalumbo@apache.org>
2019-09-20 05:19:19 -0700

    mend
    inital commit

--- sparseMatMatMul/src/main/scala/org/apache/mahout/exampleApp1/ex1.scala
+++ sparseMatMatMul/src/main/scala/org/apache/mahout/exampleApp1/ex1.scala
@@ -14,8 +14,9 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
 */
+package org.aoache.mahout
 
-import org.apache.mahout.math_
+import org.apache.mahout.math.
 
 
 class exampleApp1(val _m: Int,
