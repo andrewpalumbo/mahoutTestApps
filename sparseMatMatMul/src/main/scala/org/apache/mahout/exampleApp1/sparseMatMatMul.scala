9012210186dc695a2abe1384e0a8db340b887e55
Andrew Palumbo <apalumbo@apache.org>
2020-01-14 22:57:43 -0800

    set correct goals for scalatest

--- sparseMatMatMul/src/main/scala/org/apache/mahout/exampleApp1/sparseMatMatMul.scala
+++ sparseMatMatMul/src/main/scala/org/apache/mahout/exampleApp1/sparseMatMatMul.scala
@@ -17,10 +17,6 @@
 
 package org.apache.mahout.exampleApp1
 
-import org.apache.spark.SparkContext
-import org.apache.spark.SparkConf
-import org.apache.mahout.sparkbindings.SparkDistributedContext
-import org.apache.mahout.sparkbindings
 import org.apache.log4j.Logger
 import org.apache.mahout._
 import org.apache.mahout.math._
@@ -30,9 +26,8 @@ import math.drm._
 import scalabindings._
 import RLikeOps._
 import RLikeDrmOps._
-
-import scala.reflect
-
+import org.apache.mahout.sparkbindings.SparkDistributedContext
+import org.apache.spark.{SparkConf, SparkContext}
 
 
 class sparseMatMatMul(val _m: Int,
@@ -42,55 +37,54 @@ class sparseMatMatMul(val _m: Int,
                   val _pctDense: Double = .01,
                   val _seed: Long = 1234L) {
 
-  protected implicit var mahoutCtx: DistributedContext = _
+    protected implicit var mahoutCtx: DistributedContext = _
 
-  val log: Logger = Logger.getLogger(this.getClass())
+    val log: Logger = Logger.getLogger(this.getClass)
 
-  def timeSparseDRMMMul(m: Int = _m,
+    def timeSparseDRMMMul(m: Int = _m,
                       n: Int = _n,
                       s: Int = _s,
                       para: Int = _para,
                       pctDense: Double = _pctDense,
                       seed: Long = _seed): Long = {
 
+        val drmA = drmParallelizeEmpty(m , s, para).mapBlock(){
+        case (keys, block:Matrix) =>
+          val R =  scala.util.Random
+          R.setSeed(seed)
+          val blockB = new SparseRowMatrix(block.nrow, block.ncol)
+          blockB := {x => if (R.nextDouble < pctDense) R.nextDouble else x }
+          (keys -> blockB)
+        }
+        val drmB = drmParallelizeEmpty(s , n, para).mapBlock(){
+          case (keys, block:Matrix) =>
+            val R =  scala.util.Random
+            R.setSeed(seed + 1)
+            val blockB = new SparseRowMatrix(block.nrow, block.ncol)
+            blockB := {x => if (R.nextDouble < pctDense) R.nextDouble else x }
+            (keys -> blockB)
+        }
 
-    val drmA = drmParallelizeEmpty(m , s, para).mapBlock(){
-      case (keys,block:Matrix) =>
-        val R =  scala.util.Random
-        R.setSeed(seed)
-        val blockB = new SparseRowMatrix(block.nrow, block.ncol)
-        blockB := {x => if (R.nextDouble < pctDense) R.nextDouble else x }
-        (keys -> blockB)
+        var i: Int = 0
+        //var drmC: DrmLike[Int] = drmParallelizeEmpty(m,s,para)
+        var time = System.currentTimeMillis()
+        for (i <- 1 to 10) {
+        val drmC = (drmA %*% drmB)
+
+        // trigger computation by calling numRows()
+        drmC.numRows()
+      }
+
+      time = System.currentTimeMillis() - time
+
+      // this timer is meant as a sanity check and an apples to apples test, for comparing multiplication methods across
+      // mahout only.
+      time
     }
-    val drmB = drmParallelizeEmpty(s , n, para).mapBlock(){
-      case (keys,block:Matrix) =>
-        val R =  scala.util.Random
-        R.setSeed(seed + 1)
-        val blockB = new SparseRowMatrix(block.nrow, block.ncol)
-        blockB := {x => if (R.nextDouble < pctDense) R.nextDouble else x }
-        (keys -> blockB)
-    }
 
-    var i: Int = 0
-    //var drmC: DrmLike[Int] = drmParallelizeEmpty(m,s,para)
-    var time = System.currentTimeMillis()
-    for (i <- 1 to 10) {
-      val drmC = (drmA %*% drmB)
-
-      // trigger computation by calling numRows()
-      drmC.numRows()
-    }
-
-    time = System.currentTimeMillis() - time
-
-    // this timer is meant as a sanity check and an apples to apples test, for comparing multiplication methods across
-    // mahout only.
-    time
   }
 
-}
-
-  /*object sparseMatMatMul {
+  object sparseMatMatMul {
 
     def main(args: Array[String]): Unit = {
 
@@ -102,20 +96,20 @@ class sparseMatMatMul(val _m: Int,
       var para: Int = 6
       var pctDense: Double = 0.01
       var seed: Long = 1234L
-/**
-      case :
-        (array   (master.getClass()),
-             mxM.asInstanceOf[arg[1].getRuntime.get],
-             mxS.asInstanceOf[Int],
-             mxN.asInstanceOf[Int],
-             para.asInstanceOf[Int],
-             pctDense.asInstanceOf[Int],
-             seed.asInstanceOf[Long]) => (true, true, true, true, true, true, true)
-      case : _
-        log.fatal("Usage:" +
-   Spark       "Spark master  %s"+
-          )
-**/
+      /**
+       * case :
+       * (array   (master.getClass()),
+       *mxM.asInstanceOf[arg[1].getRuntime.get],
+       *mxS.asInstanceOf[Int],
+       *mxN.asInstanceOf[Int],
+       *para.asInstanceOf[Int],
+       *pctDense.asInstanceOf[Int],
+       *seed.asInstanceOf[Long]) => (true, true, true, true, true, true, true)
+       * case : _
+       *log.fatal("Usage:" +
+       * Spark       "Spark master  %s"+
+       * )
+       **/
 
 
       val sparkConf = new SparkConf()
@@ -123,20 +117,15 @@ class sparseMatMatMul(val _m: Int,
         .setAppName("SparseMatMatMul-example")
 
       val sctx: SparkContext = new SparkContext(sparkConf)
-      implicit val ctx= new SparkDistributedContext(sctx)
+      implicit val ctx = new SparkDistributedContext(sctx)
 
-      val sMmm = new sparseMatMatMul(mxM,
-                        mxS,
-                        mxN,
-                        para,
-                        pctDense,
-                        seed)
-
+      val sMmm = new sparseMatMatMul(mxM, mxS, mxN, para, pctDense, seed)
       val wallClockTime: Long = sMmm.timeSparseDRMMMul()
-
       val log = Logger.getLogger(sMmm.getClass)
 
-     // log.trace(String.format("tWallclock Tome to multiply drmA %*% drmB =  $L", wallClockTime))
-
+      log.trace("Wallclock Time to multiply drmA %*% drmB = %3d".format(wallClockTime))
     }
-*/}
+  }
+
+
+
